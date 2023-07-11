4bfb4fd8da444508d760f49ab60aa2c803bae81e
aking618 <ashsquads@gmail.com>
2023-07-10 19:00:16 -0500

    Initial Commit

---
+++ InstagramUIClone/InstagramUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUICloneApp.swift
+//  InstagramUIClone
+//
+//  Created by Ayren King on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
